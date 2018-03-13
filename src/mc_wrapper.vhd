dfdabc369185175d6ea50ccf25ee6be63228a2b6
Benjamin Maitland <marthinwurer@gmail.com>
2018-03-13 00:39:01 -0400

    Added a wrapper to test with

---
+++ src/mc_wrapper.vhd
@@ -0,0 +1,106 @@
+-- Benjamin Maitland
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+use work.so_eddgy.ALL;
+
+
+entity mc_wrapper is
+	port (
+		-- data from the on-chip memory (chip side)
+		A: out std_logic_vector(19 downto 0);
+		-- clock, address valid, chip enable, output enable, write enable, 
+		-- lower byte enable, upper byte enable.
+		CLK, ADV, CRE, CE, OE, WE, LB, UB: out std_logic;
+		DQ: inout std_logic_vector(15 downto 0);
+		WAIT_p : in std_logic;
+
+		-- switch data
+		inb: in std_logic_vector(7 downto 0);
+		-- raw button input
+		write_button, read_button, data_button: in std_logic;
+		-- led output
+		outb: out std_logic_vector(7 downto 0);
+
+		-- system clock
+		clock: in std_logic
+);
+end mc_wrapper;
+
+architecture behav of mc_wrapper is
+	component memory_controller 
+	port (
+		-- data from the on-chip memory (chip side)
+		A: out std_logic_vector(19 downto 0);
+		-- clock, address valid, chip enable, output enable, write enable, 
+		-- lower byte enable, upper byte enable.
+		CLK, ADV, CRE, CE, OE, WE, LB, UB: out std_logic;
+		DQ: inout std_logic_vector(15 downto 0);
+		WAIT_p : in std_logic;
+
+
+		-- processor side 
+		addr: in std_logic_vector(19 downto 0);
+		clock: in std_logic;
+		operation: in std_logic; -- if low, read; if high, write
+		enable: in std_logic; -- enable high
+		write: in std_logic_vector(15 downto 0);
+		read: out std_logic_vector(15 downto 0);
+		ready: out std_logic -- high if ready
+	);
+	end component;
+
+	component re_pulse
+	port (
+		a, clk: in std_logic;
+		o : out std_logic
+	);
+	end component;
+
+	-- addresses going off chip and into the controller
+	signal addr: std_logic_vector(19 downto 0); 
+	-- all from memory controller
+	signal operation, enable, ready: std_logic;
+	-- byte to display, byte from switches, latched address, latched data
+	signal switch_addr, data: std_logic_vector(7 downto 0);
+
+	-- I/O from controller and to chip
+	signal write, read: std_logic_vector(15 downto 0);
+	-- button pulse output
+	signal write_pressed, read_pressed, data_pressed: std_logic;
+
+
+begin
+	mc: entity work.memory_controller
+	port map (A=>A, CLK=>CLK, ADV=>ADV, CRE=>CRE, CE=>CE, OE=>OE, WE=>WE, LB=>LB, UB=>UB,
+				DQ=>Dq, WAIT_p=>WAIT_p, addr=>addr, clock=>clock, operation=>operation,
+				enable=>enable, write=>write, read=>read, ready=>ready);
+
+	wb: entity work.re_pulse
+	port map (a=>write_button, clk=>clock, o=>write_pressed);
+	rb: entity work.re_pulse
+	port map (a=>read_button, clk=>clock, o=>read_pressed);
+	db: entity work.re_pulse
+	port map (a=>data_button, clk=>clock, o=>data_pressed);
+
+	enable <= write_pressed or read_pressed;
+	operation <= write_pressed;
+	addr(19 downto 8) <= (others=>'0');
+	addr(7 downto 0) <= switch_addr;
+	write(15 downto 8) <= (others=>'0');
+	write(7 downto 0) <= data;
+
+	switch_addr <= inb;
+	outb <= read(7 downto 0);
+
+	process (clock, data_pressed) is
+	begin
+		if rising_edge(clock) then
+			if data_pressed = '1' then
+				data <= inb;
+			end if;
+		end if;
+	end process;
+				
+end behav;
+
---
+++ src/memory_controller.vhd
@@ -0,0 +1,145 @@
+-- Benjamin Maitland
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+use work.so_eddgy.ALL;
+
+
+entity memory_controller is
+	port (
+		-- data from the on-chip memory (chip side)
+		A: out std_logic_vector(19 downto 0);
+		-- clock, address valid, chip enable, output enable, write enable, 
+		-- lower byte enable, upper byte enable.
+		CLK, ADV, CRE, CE, OE, WE, LB, UB: out std_logic;
+		DQ: inout std_logic_vector(15 downto 0);
+		WAIT_p : in std_logic;
+
+
+	-- processor side 
+		addr: in std_logic_vector(19 downto 0);
+		clock: in std_logic;
+		operation: in std_logic; -- if low, read; if high, write
+		enable: in std_logic; -- enable high
+		write: in std_logic_vector(15 downto 0);
+		read: out std_logic_vector(15 downto 0);
+		ready: out std_logic -- high if ready
+	);
+end memory_controller;
+
+architecture behav of memory_controller is
+	type states is (C_READY, READING, WRITING);
+type i_state is record 
+	state: states;
+	duration: integer;
+	address: std_logic_vector(19 downto 0);
+	write_register: std_logic_vector(15 downto 0);
+	read_register: std_logic_vector(15 downto 0);
+end record;
+
+constant read_cycles: integer := 7;
+constant write_cycles: integer := 7;
+
+signal current_state, next_state: i_state;
+
+begin
+
+	main: process(current_state, addr, DQ, WAIT_p, write, operation, enable) is
+		variable n : i_state;
+		variable c : i_state;
+	begin
+		n := current_state;
+		c := current_state;
+
+		-- if ready, if enable is high, then latch the next address no matter what. 
+		if n.state = C_READY then
+			if enable = '1' then
+				n.address := addr;
+				n.duration := 0;
+
+		-- if the operation is high, then change state to write
+				if operation = '1' then 
+					n.state := WRITING;
+					n.write_register := write;
+				else
+					-- by default read
+					n.state := READING;
+				end if;
+			end if;
+		elsif n.state = WRITING then
+			n.duration := n.duration + 1;
+			if n.duration = write_cycles then
+				n.state := C_READY;
+			end if;
+			
+		else -- read state
+			n.duration := n.duration + 1;
+
+			if n.duration = read_cycles then
+				n.state := C_READY;
+				n.read_register := DQ;
+			end if;
+		end if;
+
+		-- outputs
+		read <= current_state.read_register;
+		CLK <= '0';
+
+		--A: out std_logic_vector(19 downto 0);
+		-- clock, address valid, chip enable, output enable, write enable, 
+		-- lower byte enable, upper byte enable.
+		--CLK, ADV, CRE, CE, OE, WE, LB, UB: out std_logic;
+		
+		if n.state = C_READY then
+			A <= (others => 'Z');
+			ADV <= '1';
+			LB <= '0';
+			UB <= '0';
+			CE <= '0';
+			CRE <= '0'; -- should always be low
+			OE <= '1';
+			WE <= '1';
+			ready <= '1';
+			DQ <= (others => 'Z');
+		
+		elsif n.state = WRITING then
+			A <= c.address;
+			ADV <= '0';
+			LB <= '0';
+			UB <= '0';
+			CE <= '0';
+			CRE <= '0'; -- should always be low
+			OE <= '0';
+			WE <= '1';
+			ready <= '0';
+			DQ <= c.write_register;
+		else -- reading
+			A <= c.address;
+			ADV <= '0';
+			LB <= '0';
+			UB <= '0';
+			CE <= '0'; -- should always be low
+			CRE <= '0'; -- should always be low
+			OE <= '1';
+			WE <= '0';
+			ready <= '0';
+			DQ <= (others => 'Z');
+		end if;
+
+			
+
+
+
+		next_state <= n;
+	end process;
+
+	tick: process (clock) is
+	begin
+		if rising_edge(clock) then
+			current_state <= next_state;
+		end if;
+	end process;
+
+end behav;
+
+
